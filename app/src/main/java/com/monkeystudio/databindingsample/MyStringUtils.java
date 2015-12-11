0ff118c7d679e297861e302f212a1884e73132ea
Monkey Yao <Monkey Yao>
2015-12-11 17:33:56 +0800

    first commit

---
+++ app/src/main/java/com/monkeystudio/databindingsample/MyStringUtils.java
@@ -0,0 +1,10 @@
+package com.monkeystudio.databindingsample;
+
+public class MyStringUtils {
+    public static String capitalize(final String word) {
+        if (word.length() > 1) {
+            return String.valueOf(word.charAt(0)).toUpperCase() + word.substring(1);
+        }
+        return word;
+    }
+}
